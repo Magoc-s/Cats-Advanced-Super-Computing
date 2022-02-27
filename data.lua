ff8d551394ae73ee43be783a7233fe09bb7778fd
Adam Rowland <u5799578@anu.edu.au>
2022-02-27 12:26:34 +1100

    Removed require for file that no longer exists.

--- data.lua
+++ data.lua
@@ -7,7 +7,7 @@ require("default-funcs")
 require("prototypes.collections.computing")
 require("prototypes.collections.cooling")
 -- Technology trees
-require("prototypes.cat-technologies")
+-- require("prototypes.cat-technologies")
 -- Fluids
 require("prototypes.fluids.coolants")
 require("prototypes.fluids.misc")
