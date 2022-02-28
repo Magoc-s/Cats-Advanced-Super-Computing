449e9aa4c7a84d913edb06293d1118548fed22d8
Adam Rowland <u5799578@anu.edu.au>
2022-02-28 22:07:45 +1100

    Added requires for categories

--- data.lua
+++ data.lua
@@ -3,10 +3,15 @@
 -- Vars and funcs
 require("default-vars")
 require("default-funcs")
+-- Categories
+require("prototypes.categories.crafting-category")
+require("prototypes.categories.module-category")
 -- Items, Recipes, Entities TODO: split these files into corresponding thingies
 require("prototypes.recipe.coolant-air")
 require("prototypes.recipe.compute-data")
+-- Entitities included here
 require("prototypes.entities.asc-entities")
+
 require("prototypes.collections.computing")
 require("prototypes.collections.cooling")
 -- Technology trees
@@ -15,6 +20,6 @@ require("prototypes.collections.cooling")
 require("prototypes.fluids.coolants")
 require("prototypes.fluids.misc")
 -- Modules
-require("prototypes.modules.asc-compute-modules")
+-- require("prototypes.modules.asc-compute-modules")
 -- Custom ASC modules
 require("prototypes.categories.module-category")
